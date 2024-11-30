b95a2b247929ea81f0c6fe54f9ed100c9397e939
davidebalice <davide.balice@gmail.com>
2024-11-30 17:17:24 +0100

    update

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const closeButton = document.getElementById("close-button");
 const playButton = document.getElementById("play-button");
 const pauseButton = document.getElementById("pause-button");
 const stopButton = document.getElementById("stop-button");
+const volumeButton = document.getElementById("volume-button");
+const volumeControlContainer = document.getElementById(
+  "volume-control-container"
+);
 const volumeControl = document.getElementById("volume-control");
 const infoControl = document.getElementById("info-button");
 const progressContainer = document.getElementById("progress");
@@ -11,10 +15,20 @@ const progressBar = document.getElementById("progress-bar");
 const currentTimeDisplay = document.getElementById("current-time");
 const totalTimeDisplay = document.getElementById("total-time");
 const playlistContainer = document.getElementById("playlist-container");
+const playlistButton = document.getElementById("playlist-button");
+const coverContainer = document.getElementById("cover-container");
+const coverImageContainer = document.getElementById("cover-image-container");
+const coverButton = document.getElementById("cover-button");
 const currentTrack = document.getElementById("current-track");
+const nextTrack = document.getElementById("next-track");
+const prevTrack = document.getElementById("prev-track");
+const style = document.getElementById("style").value;
+
 let currentTrackIndex = 0;
 let isPlaying = false;
 let tracks = [];
+const colorPrimary = document.getElementById("color-primary").value;
+const colorSecondary = document.getElementById("color-secondary").value;
 
 function isMobile() {
   return /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
@@ -43,6 +57,17 @@ infoControl.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
   overlay.style.display = "none";
+});
+
+volumeButton.addEventListener("click", () => {
+  if (
+    volumeControlContainer.style.display === "none" ||
+    volumeControlContainer.style.display === ""
+  ) {
+    volumeControlContainer.style.display = "block";
+  } else {
+    volumeControlContainer.style.display = "none";
+  }
 });
 
 volumeControl.addEventListener("input", () => {
@@ -90,15 +115,19 @@ async function loadPlaylist() {
     });
 
     tracks = olElement.querySelectorAll("li");
-    tracks.forEach((track) => {
+    tracks.forEach((track, index) => {
       track.addEventListener("click", () => {
         tracks.forEach((t) => {
-          t.style.color = "#f2c90a";
+          t.style.color = colorPrimary;
         });
         const src = track.getAttribute("data-src");
         audioPlayer.src = src;
         audioPlayer.play();
-        track.style.color = "#c1a316";
+        track.style.color = colorSecondary;
+        currentTrackIndex = index;
+        isPlaying = true;
+        loadCover();
+        showCover();
 
         const titleElement = track.querySelector(
           ".playlist-content .playlist-title"
@@ -114,7 +143,6 @@ async function loadPlaylist() {
           const column = index < itemsPerColumn ? 1 : 2;
           item.style.gridRow = row;
           item.style.gridColumn = column;
-          currentTrackIndex = index;
         });
       } else {
         tracks.forEach((item, index) => {
@@ -127,7 +155,65 @@ async function loadPlaylist() {
   }
 }
 
-loadPlaylist();
+async function loadCover() {
+  try {
+    const playlistResponse = await fetch("playlist.json");
+    const playlist = await playlistResponse.json();
+
+    const currentTrackData = playlist[currentTrackIndex];
+
+    let coverImage;
+
+    if (currentTrackData) {
+      if (style == 2) {
+        coverImage = currentTrackData.thumb;
+      } else {
+        coverImage = currentTrackData.cover;
+      }
+
+      const title = currentTrackData.title;
+      coverImageContainer.innerHTML = `<img src="${coverImage}" alt="Cover" class="track-cover" /><div id="cover-title">${title}</div><img src="${playButton.src}" id="cover-play">`;
+    }
+
+    const coverPlay = document.getElementById("cover-play");
+
+    coverPlay.addEventListener("click", () => {
+      if (isPlaying) {
+        audioPlayer.pause();
+        isPlaying = false;
+        coverPlay.src = pauseButton.src;
+      } else {
+        audioPlayer.play();
+        isPlaying = true;
+        coverPlay.src = playButton.src;
+      }
+    });
+  } catch (error) {
+    console.error("Error loading cover:", error);
+  }
+}
+
+function showCover() {
+  playlistContainer.style.display = "none";
+  coverContainer.style.display = "flex";
+}
+
+function hideCover() {
+  playlistContainer.style.display = "block";
+  coverContainer.style.display = "none";
+}
+
+nextTrack.addEventListener("click", () => {
+  currentTrackIndex = (currentTrackIndex + 1) % tracks.length;
+  tracks[currentTrackIndex].click();
+  loadCover();
+});
+
+prevTrack.addEventListener("click", () => {
+  currentTrackIndex = (currentTrackIndex - 1 + tracks.length) % tracks.length;
+  tracks[currentTrackIndex].click();
+  loadCover();
+});
 
 audioPlayer.addEventListener("ended", () => {
   currentTrackIndex++;
@@ -165,3 +251,29 @@ progressContainer.addEventListener("click", (event) => {
   const newTime = (clickX / progressWidth) * audioPlayer.duration;
   audioPlayer.currentTime = newTime;
 });
+
+playlistButton.addEventListener("click", (event) => {
+  hideCover();
+});
+
+coverButton.addEventListener("click", (event) => {
+  showCover();
+});
+
+loadPlaylist().then(() => {
+  loadCover();
+});
+
+/*
+<canvas id="pixel-canvas" width="400" height="400"></canvas>
+<img id="source-image" src="image-40px.jpg" style="display: none;" />
+const canvas = document.getElementById("pixel-canvas");
+const ctx = canvas.getContext("2d");
+const img = document.getElementById("source-image");
+
+img.onload = () => {
+  // Disegna l'immagine originale di 40x40 pixel
+  ctx.imageSmoothingEnabled = false; // Disattiva lo smoothing
+  ctx.drawImage(img, 0, 0, 40, 40, 0, 0, 400, 400); // Scala a 400x400
+};
+*/
